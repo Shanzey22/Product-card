734e08b538beb2afc0b8ba7012042f9d58d39302
Shanzey22 <shanzeyshaikh22@gmail.com>
2023-10-08 17:21:57 +0530

    first commit

---
+++ src/content.js
@@ -0,0 +1,31 @@
+const productData = [
+    {
+        id:1,
+        name:"Boat-Boom-headphone-wired-bass-heads",
+        image :"https://www.sathya.in/media/38253/catalog/boat-boom-headphone-wired-bass-heads-910.jpg?size=600",
+        productType:"openBackHeadphones",
+        price:300,
+        rating:5,
+        timeLeft:27,
+        totalSales:7479,
+    },
+    {
+        id:2,
+        name:"Best Mobile Phones",
+        image:"https://savyour.com.pk/products/wp-content/uploads/2022/10/xiaomi-11-t-1.png",
+        price:1200,
+        rating:5,
+        timeLeft:24,
+        totalSales:6869
+    },
+    {
+        id:3,
+        name:" Waterproof Sports Watch Quartz ",
+        image:"https://www.fireboltt.com/cdn/shop/products/rocket-black_1_400x.png?v=1671794799",
+        price:1200,
+        rating:5,
+        timeLeft:24,
+        totalSales:6869
+    },
+];
+export default productData;
